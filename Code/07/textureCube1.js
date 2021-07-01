45d6a7854b0b7670f6f44f13132d5115c091eeaf
1337968347 <1337968347@qq.com>
2021-07-02 03:29:51 +0800

    :star: Normal vector

--- Code/07/textureCube1.js
+++ Code/07/textureCube1.js
@@ -1,9 +1,7 @@
-
-
 var canvas;
 var gl;
 
-var numVertices  = 36;
+var numVertices = 36;
 
 var texSize = 64;
 
@@ -14,36 +12,31 @@ var texCoordsArray = [];
 var normalsArray = [];
 
 var texture;
+var modelView;
 
-var texCoord = [
-    vec2(0, 0),
-    vec2(0, 1),
-    vec2(1, 1),
-    vec2(1, 0)
-];
+var texCoord = [vec2(0, 0), vec2(0, 1), vec2(1, 1), vec2(1, 0)];
 
 var vertices = [
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5, -0.5, -0.5, 1.0 )
+  vec4(-0.5, -0.5, 0.5, 1.0),
+  vec4(-0.5, 0.5, 0.5, 1.0),
+  vec4(0.5, 0.5, 0.5, 1.0),
+  vec4(0.5, -0.5, 0.5, 1.0),
+  vec4(-0.5, -0.5, -0.5, 1.0),
+  vec4(-0.5, 0.5, -0.5, 1.0),
+  vec4(0.5, 0.5, -0.5, 1.0),
+  vec4(0.5, -0.5, -0.5, 1.0),
 ];
 
-
 var vertexColors = [
-    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-    vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
-    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
-];    
+  vec4(0.0, 0.0, 0.0, 1.0), // black
+  vec4(1.0, 0.0, 0.0, 1.0), // red
+  vec4(1.0, 1.0, 0.0, 1.0), // yellow
+  vec4(0.0, 1.0, 0.0, 1.0), // green
+  vec4(0.0, 0.0, 1.0, 1.0), // blue
+  vec4(1.0, 0.0, 1.0, 1.0), // magenta
+  vec4(0.0, 1.0, 1.0, 1.0), // white
+  vec4(0.0, 1.0, 1.0, 1.0), // cyan
+];
 
 var xAxis = 0;
 var yAxis = 1;
@@ -53,140 +46,147 @@ var theta = [45.0, 45.0, 45.0];
 
 var thetaLoc;
 
-function configureTexture( image ) {
-    texture = gl.createTexture();
-    gl.bindTexture( gl.TEXTURE_2D, texture );
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, 
-         gl.RGB, gl.UNSIGNED_BYTE, image );
+function configureTexture(image) {
+  texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
 
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
-                      gl.LINEAR );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
-    
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 }
-
 
 function quad(a, b, c, d) {
+  var t1 = subtract(vertices[b], vertices[a]);
+  var t2 = subtract(vertices[c], vertices[b]);
+  var normal = vec4(cross(t1, t2));
 
-    var t1 = subtract(vertices[b], vertices[a]);
-    var t2 = subtract(vertices[c], vertices[b]);
-    var normal = cross(t1, t2);
-    var normal = vec3(normal);
+  pointsArray.push(vertices[a]);
+  texCoordsArray.push(texCoord[0]);
+  normalsArray.push(normal);
 
-     pointsArray.push(vertices[a]);
-     texCoordsArray.push(texCoord[0]);
-     normalsArray.push(normal)
+  pointsArray.push(vertices[b]);
+  texCoordsArray.push(texCoord[1]);
+  normalsArray.push(normal);
 
-     pointsArray.push(vertices[b]); 
-     texCoordsArray.push(texCoord[1]); 
-     normalsArray.push(normal)
+  pointsArray.push(vertices[c]);
+  texCoordsArray.push(texCoord[2]);
+  normalsArray.push(normal);
 
-     pointsArray.push(vertices[c]); 
-     texCoordsArray.push(texCoord[2]); 
-     normalsArray.push(normal)
-   
-     pointsArray.push(vertices[a]); 
-     texCoordsArray.push(texCoord[0]); 
-     normalsArray.push(normal)
+  pointsArray.push(vertices[a]);
+  texCoordsArray.push(texCoord[0]);
+  normalsArray.push(normal);
 
-     pointsArray.push(vertices[c]); 
-     texCoordsArray.push(texCoord[2]); 
-     normalsArray.push(normal)
+  pointsArray.push(vertices[c]);
+  texCoordsArray.push(texCoord[2]);
+  normalsArray.push(normal);
 
-     pointsArray.push(vertices[d]); 
-     texCoordsArray.push(texCoord[3]); 
-     normalsArray.push(normal)  
+  pointsArray.push(vertices[d]);
+  texCoordsArray.push(texCoord[3]);
+  normalsArray.push(normal);
 }
 
-
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+function colorCube() {
+  quad(1, 0, 3, 2);
+  quad(2, 3, 7, 6);
+  quad(3, 0, 4, 7);
+  quad(6, 5, 1, 2);
+  quad(4, 5, 6, 7);
+  quad(5, 4, 0, 1);
 }
-
 
 window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
 
-    canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
-    gl.enable(gl.DEPTH_TEST);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-    
-    colorCube();
+  gl.enable(gl.DEPTH_TEST);
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    
-    var tBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
-    
-    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
-    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vTexCoord );
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
-    var NBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, NBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
-    
-    var vNormal = gl.getAttribLocation( program, "vNormal" );
-    gl.vertexAttribPointer( vNormal, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vNormal );
+  colorCube();
 
-    //
-    // Initialize a texture
-    //
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
-    //var image = new Image();
-    //image.onload = function() { 
-     //   configureTexture( image );
-    //}
-    //image.src = "SA2011_black.gif"
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
 
+  var tBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
 
-    var image = document.getElementById("texImage");
- 
-    configureTexture( image );
+  var vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+  gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vTexCoord);
 
-    thetaLoc = gl.getUniformLocation(program, "theta"); 
-    
-    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
-    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
-    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
-       
-    render();
- 
-}
+  var NBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, NBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
 
-var render = function(){
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    theta[axis] += 1.0;
-    gl.uniform3fv(thetaLoc, flatten(theta));
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
-    requestAnimFrame(render);
-}
+  var vNormal = gl.getAttribLocation(program, "vNormal");
+  gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vNormal);
+
+  //
+  // Initialize a texture
+  //
+
+  //var image = new Image();
+  //image.onload = function() {
+  //   configureTexture( image );
+  //}
+  //image.src = "SA2011_black.gif"
+
+  var image = document.getElementById("texImage");
+
+  configureTexture(image);
+
+  thetaLoc = gl.getUniformLocation(program, "theta");
+
+  document.getElementById("ButtonX").onclick = function () {
+    axis = xAxis;
+  };
+  document.getElementById("ButtonY").onclick = function () {
+    axis = yAxis;
+  };
+  document.getElementById("ButtonZ").onclick = function () {
+    axis = zAxis;
+  };
+
+  render();
+};
+
+var render = function () {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  theta[axis] += 1.0;
+
+  modelView = mat4();
+  modelView = mult(modelView, rotate(theta[xAxis], [1, 0, 0]));
+  modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0]));
+  modelView = mult(modelView, rotate(theta[zAxis], [0, 0, 1]));
+  gl.uniformMatrix4fv(
+    gl.getUniformLocation(program, "modelViewMatrix"),
+    false,
+    flatten(modelView)
+  );
+
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+  requestAnimFrame(render);
+};
